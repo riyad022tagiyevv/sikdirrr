5dde1496d63088cbb3e9d1171e8f4a521a0141f8
PornoHup <107538335+PornoHup@users.noreply.github.com>
2022-06-24 17:43:11 +0400

    Update moon.py

--- moon.py
+++ moon.py
@@ -29,9 +29,9 @@ async def start_(client: Client, message: Message):
     f"""● **🇦🇿🐊 **Salam** {message.from_user.mention}\n\n**ℹ️ Mən music endirmə botuyam istədiyin musiqini yükləyəbilərsiniz**\n\n**✅ Yardım üçün** /help **düyməsini basın**""",
         reply_markup=InlineKeyboardMarkup(
             [[
-                    InlineKeyboardButton('Support Grup ❄️', url=f'https://t.me/{Config.GROUP}'),
+                    InlineKeyboardButton('Support Qrupu 🤍🐊', url=f'https://t.me/Karabakhteamm{Config.GROUP}'),
                   ],[
-                    InlineKeyboardButton('Playlist 🎵', url=f'https://t.me/{Config.PLAYLIST_NAME}')
+                    InlineKeyboardButton('Playlist 🎵', url=f'https://t.me/RiyaddBlog{Config.PLAYLIST_NAME}')
                 ]
             ]
         )
